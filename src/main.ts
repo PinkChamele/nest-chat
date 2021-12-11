226befbd75da77199f18b23be9bb37a33cfc4e83
oleksandr.makarevych <oleksandr.makarevych@onix-systems.com>
2021-12-11 15:53:22 +0200

    feat: test hook

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,14 @@ import * as session from 'express-session';
 import { join } from 'path';
 
 import { NestApplication, NestFactory } from '@nestjs/core';
-import { VersioningType, Logger } from '@nestjs/common';
+import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import AppModule from './app.module';
 import AllExceptionsFilter from './components/common/filters/all-exceptions.filter';
 
-// add https://github.com/nestjs/nest/tree/master/sample/10-fastify/src/common
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule);
   const configService = app.get(ConfigService);
-  Logger.log(configService.get('sendgrid.host'));
   app.enableVersioning({
     type: VersioningType.URI,
   });
